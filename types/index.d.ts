1b572f0fbcfc108e8dbf226d47e81cbf2ef31a96
sverweij <sverweij@users.noreply.github.com>
2018-01-21 20:58:02 +0100

    feature(types): adds typescript type definition

---
+++ types/index.d.ts
@@ -0,0 +1,18 @@
+
+declare const tryRequire:
+/**
+ * returns the (resolved) module identified by pModuleName:
+ * - if it is available, and
+ * - it satisfies the semantic version range specified by pSemVer
+ *
+ * returns false in all other cases
+ *
+ * @param  {string} pModuleName the name of the module to resolve
+ * @param  {string} pSemVer     (optional) a semantic version (range)
+ * @return {object}             the (resolved) module identified by pModuleName
+ *                              or false
+ */
+(pModuleName: string, pSemVer?: string) => boolean | object;
+export = tryRequire;
+
+// export default (pModuleName: string, pSemVer?: string) => {}:object;

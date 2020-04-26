d48871e1e749d47014bb9970ef04bfe7278739cf
sverweij <sverweij@users.noreply.github.com>
2020-04-26 21:07:25 +0200

    build(npm): de-support node 8 & 11
    also :arrow_up: all dependencies we didn't up because they already desupported node 8

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,39 +4,43 @@ const rcFixture = require("../test/rc-fixture");
 const betaFixture = require("../test/rc-fixture");
 
 describe("tryRequire", () => {
-  test("returns false for unresolvable modules", () => {
+  it("returns false for unresolvable modules", () => {
     expect(tryRequire("thispackage-is-not-there")).toBe(false);
   });
 
-  test("returns the module if it is resolvable", () => {
-    expect(tryRequire("semver")).toEqual(semver);
+  it("returns the module if it is resolvable", () => {
+    expect(tryRequire("semver")).toStrictEqual(semver);
   });
 
-  test("returns the module if it is resolvable and satisfies specified semver", () => {
-    expect(tryRequire("semver", ">=5.0.0 <7.0.0")).toEqual(semver);
+  it("returns the module if it is resolvable and satisfies specified semver", () => {
+    expect(tryRequire("semver", ">=5.0.0 <8.0.0")).toStrictEqual(semver);
   });
 
-  test("returns the module if it is resolvable and satisfies specified semver (with rc postfix)", () => {
-    expect(tryRequire("../test/rc-fixture", ">=2.0.0 <4.0.0")).toEqual(
+  it("returns the module if it is resolvable and satisfies specified semver (with rc postfix)", () => {
+    expect(tryRequire("../test/rc-fixture", ">=2.0.0 <4.0.0")).toStrictEqual(
       rcFixture
     );
   });
 
-  test("returns false if it is resolvable but does not satisfy specified semver (with rc postfix)", () => {
-    expect(tryRequire("../test/rc-fixture", ">=2.0.0 <3.0.0")).toEqual(false);
+  it("returns false if it is resolvable but does not satisfy specified semver (with rc postfix)", () => {
+    expect(tryRequire("../test/rc-fixture", ">=2.0.0 <3.0.0")).toStrictEqual(
+      false
+    );
   });
 
-  test("returns the module if it is resolvable and satisfies specified semver (with beta postfix)", () => {
-    expect(tryRequire("../test/beta-fixture", ">=2.0.0 <4.0.0")).toEqual(
+  it("returns the module if it is resolvable and satisfies specified semver (with beta postfix)", () => {
+    expect(tryRequire("../test/beta-fixture", ">=2.0.0 <4.0.0")).toStrictEqual(
       betaFixture
     );
   });
 
-  test("returns false if it is resolvable but does not satisfy specified semver (with beta postfix)", () => {
-    expect(tryRequire("../test/beta-fixture", ">=2.0.0 <3.0.0")).toEqual(false);
+  it("returns false if it is resolvable but does not satisfy specified semver (with beta postfix)", () => {
+    expect(tryRequire("../test/beta-fixture", ">=2.0.0 <3.0.0")).toStrictEqual(
+      false
+    );
   });
 
-  test("returns false if it is resolvable but doesn't satisfy the specified semver", () => {
-    expect(tryRequire("semver", "<5.0.0")).toEqual(false);
+  it("returns false if it is resolvable but doesn't satisfy the specified semver", () => {
+    expect(tryRequire("semver", "<5.0.0")).toStrictEqual(false);
   });
 });

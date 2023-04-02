3c870908be873e1e36833b8736040dcbc6ac8849
sverweij <sverweij@users.noreply.github.com>
2023-04-02 21:29:16 +0200

    6.2.1

--- dist/try-import.mjs
+++ dist/try-import.mjs
@@ -1,10 +1,12 @@
 import path from "path/posix";
+import { createRequire } from "module";
 import { coerce, satisfies } from "semver";
 const LOCAL_MODULE_RE = /^[.]{1,2}($|\/.*)/g;
 const ABSOLUTE_MODULE_RE = /^\/.*/g;
 const PACKAGE_RE = "[^/]+";
 const SCOPED_PACKAGE_RE = "@[^/]+(/[^/]+)";
 const ROOT_MODULE_RE = new RegExp(`^(${SCOPED_PACKAGE_RE}|${PACKAGE_RE})`, "g");
+const require = createRequire(import.meta.url);
 function extractRootModuleName(pModuleName) {
     if (pModuleName.match(LOCAL_MODULE_RE) ||
         pModuleName.match(ABSOLUTE_MODULE_RE)) {
@@ -14,9 +16,9 @@ function extractRootModuleName(pModuleName) {
         return (pModuleName.match(ROOT_MODULE_RE) || []).shift();
     }
 }
-async function getVersion(pModuleName) {
-    const lManifest = await import(path.join(extractRootModuleName(pModuleName), "package.json"), { assert: { type: "json" } });
-    return lManifest.default.version;
+function getVersion(pModuleName) {
+    const lManifest = require(path.join(extractRootModuleName(pModuleName), "package.json"));
+    return lManifest.version;
 }
 export default async function tryImport(pModuleName, pSemanticVersion) {
     let lReturnValue = false;
@@ -24,7 +26,7 @@ export default async function tryImport(pModuleName, pSemanticVersion) {
         const lModule = await import(pModuleName);
         lReturnValue = lModule.default;
         if (pSemanticVersion) {
-            const lVersion = await getVersion(pModuleName);
+            const lVersion = getVersion(pModuleName);
             const lCoerced = coerce(lVersion);
             if (lVersion &&
                 lCoerced &&
